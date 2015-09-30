bceec44e0f2a95bf90da65cc36652251dc21cdc2
Zachary Fraser <zacharyfraser@Zacharys-MacBook-Pro.local>
2015-09-30 11:09:35 +1000

    calculations beginning

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -67,12 +67,11 @@ var lights = [
 },
 {
   name: '12V 50W MR16 GU5.3 Halogen',
-  wattage: 50,
-  life: 4000,
+  wattage: 50,
+  life: 4000,
   cost: 5.00
 }
 ];
-
 var leds = [
 {
   name: '240V 4W Candle LED',
@@ -149,19 +148,97 @@ var leds = [
 ];
 
 
-//show the result of the lights field times the hours field
-// result: function(lights, hours) {
-//   this.result.push({
-//     light: lights,
-//     hours: hours,
-//   })
-//   return lights * hours;
-// };
 
-// console.log(result)
-
-
-
-
-
-
+  // for the jquery array objects
+  var $leds = leds;
+  var $lights = lights;
+  // Declare Global values hours, number lights other wise it doesn't work
+  var $Hours = +$('#Hours').val();
+  var $Number_lights = +$('#Number_lights').val();
+  // Declare Glabal fixed values
+  var $CO2_AVG = 0.000979333;
+  var $year = 365;
+  var $KWH_Cost = 0.18;
+  // declare array object values wattage cost and life
+  var $halogenwattage = lights.wattage;
+  var $ledswattage = leds.wattage;
+  var $halogenlife = lights.life;
+  var $ledlife = leds.life;
+  var $halogencost = lights.cost;
+  var $ledcost = leds.cost;
+  var halogenObj;
+  var ledObj;
+$(document).ready(function () {
+  
+  var calculateEnergyUsage = function () {
+    // So you now have access to the correct LED and halogen lights
+      // halogenObj
+      // ledObj
+      // halogenObj.wattage
+    // Get the hours erb input
+    var $Hours = +$('#Hours').val();
+    // Get the number of lights erb input
+    var $Number_lights = +$('#Number_lights').val();
+    // Calculate yearly POWER CONSUMPTION
+    // Number of lights * hours per day * days in year * Power per light divided by 100
+    var $total_power = $Hours * $Number_lights * $year * $KWH_Cost/100;
+    // Calculate yearly COST
+    // total power times cost per KWH
+    var $total_cost = $total_power * $KWH_Cost;
+    // Calculate yearly CO EMISSIONS
+    // total power times au average emmissions
+    var $total_emssions = $total_power * $CO2_AVG;
+    // Trying to render in <div><p><span> RESULT HALOGEN </span>span> RESULT LED </span>span> DIFFERENCE </span></p></div>
+    // renders the halogen results on home.html.erb
+    $('.halogen_render', '.lifespan').html($total_power);
+  }
+  calculateEnergyUsage();
+  $("input[type=number]").on("change", function () {
+    calculateEnergyUsage();
+  });
+  /////////////////
+  // Trying to render the matching halogen wattage 
+  for (var e = 0; e < lights.length; e++) {
+    if (lights[e].name == $('.halogen').val() ) {
+      var $wattage = lights[e].wattage;
+      var $p = $('<p><span class="wattage">' + light.wattage + '<span></p>');
+      $p.data('index', e);
+      $p.appendTo('.halogen_render', '.wattage');
+    }
+  }
+  console.log($wattage);
+  $('.wattage').on('change', function () {
+    var index = $(this).find(':selected').data('index');
+    var info = lights[index];
+    console.log(info);
+  });
+  ///////////////
+  // Sets the Halogen options list from the array
+  for (var i = 0; i < lights.length; i++) {
+    var light = lights[i];
+    var $option = $('<option>' + light.name + '</option>');
+    $option.data('index', i);
+    $option.appendTo('.halogen');
+  }
+  // Listens for changes on class Halogen, stores index
+  $('.halogen').on('change', function () {
+    var index = $(this).find(':selected').data('index');
+    var info = lights[index];
+    halogenObj = info;
+    console.log(info);
+  });
+  // Sets the Leds options list from the array
+  for (var j = 0; j < leds.length; j++ ) {
+    var led = leds[j];
+    var $option = $('<option>' + led.name + '</option>');
+    $option.data('index', j);
+    $option.appendTo('.led');
+  }
+  // Listens for changes on class Led, stores index
+  $('.led').on('change', function () {
+    var index = $(this).find(':selected').data('index');
+    var info = leds[index];
+    ledObj = info;
+    console.log(info);
+  })
+});
