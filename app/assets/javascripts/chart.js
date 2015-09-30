3aeb391e322f94e5bdadfe45ad372909e3c8742c
Zachary Fraser <zacharyfraser@Zacharys-MacBook-Pro.local>
2015-10-01 08:32:05 +1000

    update chart

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,19 +1,33 @@
-      var riceData = {
+      var lightData = {
         labels : ["January","February","March","April","May","June"],
         datasets :
          [
             {
-              fillColor : "rgba(172,194,132,0.4)",
+              fillColor : "rgba(152,154,152,0.4)",
               strokeColor : "#ACC26D",
               pointColor : "#fff",
               pointStrokeColor : "#9DB86D",
-              data : [203000,15600,99000,25100,30500,24700]
+              data : [203000,18600,99000,2100,3000,24700]
+            },
+            {
+              fillColor : "rgba(252,254,102,0.4)",
+              strokeColor : "#ACC26D",
+              pointColor : "#fff",
+              pointStrokeColor : "#9DB86D",
+              data : [103000,20600,99000,28100,20500,24700]
+            },
+            {
+              fillColor : "rgba(72,194,132,0.4)",
+              strokeColor : "#ACC26D",
+              pointColor : "#fff",
+              pointStrokeColor : "#9DB86D",
+              data : [153000,10600,99000,20100,20500,29700]
             }
          ]
         }
 
 $(document).ready(function () {
-  var rice = document.getElementById('rice').getContext('2d');
-  new Chart(rice).Bar(riceData);
+  var lights = document.getElementById('lights').getContext('2d');
+  new Chart(lights).Bar(lightData);
 });
 
